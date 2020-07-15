0d202e8ec2b2d22f1e1afdfdd938177e7b0546d8
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-07-15 18:32:50 +0300

    :sparkles: Add scrollview

--- dirsize.py
+++ dirsize.py
@@ -1,6 +1,54 @@
 import os
 import time
-from tkinter import *  # you should install it
+from tkinter import *
+
+
+class VerticalScrolledFrame(Frame):
+    """A pure Tkinter scrollable frame that actually works!
+
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+    """
+
+    def __init__(self, parent, *args, **kw):
+        Frame.__init__(self, parent, *args, **kw)
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+
+        canvas.bind('<Configure>', _configure_canvas)
 
 
 def goUp():
@@ -57,27 +105,26 @@ def humanReadableSize(num, suffix='B'):
 
 
 root = Tk()
-frame = Frame(root)
+frame = VerticalScrolledFrame(root)
 frame.pack()
 changed = True
 sizes = {}
 
-gu = Button(root, text="..", width=30, command=goUp)
-gu.pack()
-
 while True:
     if changed:
-        all = os.listdir()
+        all_files = os.listdir()
         frame.destroy()
-        frame = Frame(root)
+        frame = VerticalScrolledFrame(root)
         frame.pack()
-        for item in all:
+        gu = Button(frame.interior, text="..", width=30, bg="white", command=goUp)
+        gu.pack()
+        for item in all_files:
             print(item)
             name = str(item)
             # name.set(str(item))
             # Button(frame, width=30, bg="white", text=name, command = lambda: goDown(this.text)).pack()
             labelText = " ".join([name, humanReadableSize(getSize(name))])
-            b = Button(frame, width=30, bg="white", text=labelText)
+            b = Button(frame.interior, width=30, bg="white", text=labelText)
             b.config(command=lambda x=name: goDown(x))
             b.pack()
         changed = False
